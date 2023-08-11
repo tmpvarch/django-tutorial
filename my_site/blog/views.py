2cc4ae653a8715caeb7b7dafedfaf03a6942f3dd
Сергей Дубров <tmpvarcher@outlook.com>
2023-08-11 16:24:43 +0500

    step two

--- my_site/blog/views.py
+++ my_site/blog/views.py
@@ -1,24 +1,10 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from .models import Post
 
-posts = [
-    {
-        'author': 'Администратор',
-        'title': 'Это первый пост',
-        'content': 'Содержание первого поста',
-        'date_posted': '11 августа, 2023'
-    },
-    {
-        'author': 'Пользователь',
-        'title': 'Это второй пост',
-        'content': 'Подробное содержание второго поста',
-        'date_posted': '12 августа, 2023'
-    }
-]
 
 def home(request):
     context = {
-        'posts': posts
+        'posts': Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
 
--- my_site/my_site/settings.py
+++ my_site/my_site/settings.py
@@ -32,6 +32,8 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'blog.apps.BlogConfig',
+    'users.apps.UserConfig',
+    'crispy_forms',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -118,7 +120,13 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+LOGIN_REDIRECT_URL = 'blog-home'
+LOGIN_URL = 'login'
